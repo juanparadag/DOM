04e478d88c141198bccd46cf03313f9e51d01bde
Juan <paradasebastian32@gmail.com>
2024-12-04 23:50:06 -0500

    Update 2.2

--- script.js
+++ script.js
@@ -9,15 +9,25 @@ const boton2 = document.getElementById("JavaScript"); // Botón "Ver Javascript"
 const favicon = document.getElementById("logo");// Ícono de la página (favicon, etiqueta <link>)
 const imagenDelContenedor = document.createElement("img")
 const contenedorImagen = document.getElementById("div-imagen")
+const botonRecargarPagina = document.createElement("button")
 
+function recargarPagina(lenguaje){
+
+    botonRecargarPagina.className = "botones"
+    botonRecargarPagina.innerText = "Atras"
+    lenguaje.replaceWith(botonRecargarPagina)
+    botonRecargarPagina.addEventListener("click",() =>{
+        location.reload();
+    })
+}
 
 contenedorImagen.appendChild(imagenDelContenedor)
-function verPython() {
-    
-   
+function verPython() {   
     imagenDelContenedor.src = "images/What-is-Python-3.12_11zon.webp"
     imagenDelContenedor.width = "180"
     imagenDelContenedor.setAttribute("alt", "Python");
+    imagenDelContenedor.loading = "lazy"
+
     title.innerText = "Python";
     tituloPrincipal.innerText = "Python";
     title.innerHTML = "<em>Python</em>";
@@ -33,12 +43,15 @@ function verPython() {
     // Cambiamos el comentario y su color
     comentario.innerText = "Python es un lenguaje versátil y poderoso.";
     comentario.style.color = "white"; // Color específico para Python
+    recargarPagina(boton)
+    recargarPagina(boton2)
 }
 
 function verJavaScript() {
 
     imagenDelContenedor.src = "/images/descargajs.jpg"
     imagenDelContenedor.width = "100"
+    imagenDelContenedor.loading = "lazy"
 
     title.innerText = "Javascript";
     tituloPrincipal.innerText = "Javascript";
@@ -51,7 +64,8 @@ function verJavaScript() {
     favicon.href = "/images/descargajs.jpg" 
     comentario.innerText = "Javascript es clave para el desarrollo web.";
     comentario.style.color = "black"; // Color específico para Javascript
-   
+    recargarPagina(boton2)
+    recargarPagina(boton)
 }
 
 
